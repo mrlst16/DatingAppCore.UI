943179a9fc4c3a58f7a993adf923b4c7e5b7aa52
LAPTOP-R0OK2Q7S\matt <mrlst16@mail.rmu.edu>
2019-05-04 17:12:14 -0400

    Adding a navigation bar

--- datingappui/src/App.js
+++ datingappui/src/App.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import LoginButton from '../src/components/Login'
-import Lab from './components/Lab';
+import Login from '../src/components/Login'
+import AnotherComponent from '../src/components/AnotherComponent'
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom' 
+import {Navbar, Nav, NavDropdown, Form} from 'react-bootstrap'
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-        
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+        </Navbar>;
+        <Router>
+          <div id="body">
+            <Route path="/login" component={Login} />
+            <Route path="/another" component={AnotherComponent} />
+          </div>
+        </Router>
       </div>
     );
   }
